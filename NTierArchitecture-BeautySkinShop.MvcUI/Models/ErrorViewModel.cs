913b6529831ed347eeeac37c05d841d78dafcee2
Umut <uer22231@gmail.com>
2024-08-01 20:25:59 +0300

    Add project files.

---
+++ NTierArchitecture-BeautySkinShop.MvcUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NTierArchitecture_BeautySkinShop.MvcUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
